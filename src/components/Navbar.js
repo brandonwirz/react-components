e2b31d8a0fb6945d2941b82e93dad3826292877b
Brandon Wirz <bwirz75@gmail.com>
2022-07-01 14:54:59 -0600

    updating styling adding className

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React from "react"
 import reactLogo from '../images/react-icon-small.png'; 
-
+      
 export default function Navbar() {
     return (
-        <nav>s
-            <img src={reactLogo} alt="reactlogo"/>
-            <h3>ReactFacts</h3>
-            <h4>React Course - Project 1</h4>
-        </nav>
+      <nav>
+       <img src={reactLogo} className="nav-icon" alt="reactlogo"/>                     
+       <h3 className="nav-logo-text">React Facts</h3>
+       <h4 className="nav-title">React</h4>
+      </nav>
     )
 }
